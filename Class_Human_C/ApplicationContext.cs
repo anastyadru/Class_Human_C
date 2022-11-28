63790f1355f67b3a31bb61d9ad85c809eb2bbab8
anastya_dru <adruts@mail.ru>
2022-11-28 15:04:09 +0300

    https://github.com/anastyadru/Class_Human_C.git

---
+++ Class_Human_C/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Class_Human_C
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Human> Humans { get; set; } = null!;
+        public ApplicationContext(DbContextOptions<ApplicationContext>options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
