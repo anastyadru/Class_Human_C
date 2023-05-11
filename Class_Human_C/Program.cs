10463ce58394df88111d23005aaaebfb948fa039
Nastia <adruts@mail.ru>
2023-05-11 13:02:56 +0300

    https://github.com/anastyadru/Class_Human_C.git

--- Class_Human_C/Program.cs
+++ Class_Human_C/Program.cs
@@ -15,7 +15,84 @@ namespace Class_Human_C
             
             switch (action)
             {
+                case 1:
+                    
+                    Console.WriteLine("Введите фамилию: ");
+                    var surname = Console.ReadLine();
             
+                    Console.WriteLine("Введите имя: ");
+                    var name = Console.ReadLine();
+            
+                    Console.WriteLine("Введите отчество: ");
+                    var patronymic = Console.ReadLine();
+            
+                    Console.WriteLine("Введите дату рождения в формате ДД.ММ.ГГГГ: ");
+                    var birth = Convert.ToDateTime(Console.ReadLine());
+                    
+                    Console.Write("Выберите, кого добавить:\n1) student \n2) employee \n3) driver \nВаше решение: ");
+                    var a = int.Parse(Console.ReadLine());
+            
+                    switch (a)
+                    {
+                        case 1: 
+                            
+                            Console.Write("Введите факультет");
+                            var faculty = Console.ReadLine();
+
+                            Console.Write("Введите курс");
+                            var course = int.Parse(Console.ReadLine());
+
+                            Console.Write("Введите группу");
+                            var group = int.Parse(Console.ReadLine());
+
+                            Student student = new Student(surname, name, patronymic, birth, faculty, course, group);
+
+                            people.Add(student);
+
+                            break;
+
+                        case 2:
+                            
+                            Console.Write("Введите название компании");
+                            var company = Console.ReadLine();
+
+                            Console.Write("Введите заработную плату");
+                            var salary = int.Parse(Console.ReadLine());
+
+                            Console.Write("Введите опыт работы");
+                            var experience = int.Parse(Console.ReadLine());
+
+                            Employee employee = new Employee(surname, name, patronymic, birth, company, salary, experience);
+
+                            people.Add(employee);
+                    
+                            break;
+
+                        case 3:
+                            
+                            Console.Write("Введите название компании");
+                            var company = Console.ReadLine();
+
+                            Console.Write("Введите заработную плату");
+                            var salary = int.Parse(Console.ReadLine());
+
+                            Console.Write("Введите опыт работы");
+                            var experience = int.Parse(Console.ReadLine());
+
+                            Console.Write("Введите бренд");
+                            var brand = Console.ReadLine();
+
+                            Console.Write("Введите модель");
+                            var model = Console.ReadLine();
+
+                            Driver driver = new Driver(surname, name, patronymic, birth, company, salary, experience, brand, model);
+                    
+                            people.Add(driver);
+                    
+                            break;
+                    }
+                    
+                    
             }
         }
         
@@ -23,79 +100,9 @@ namespace Class_Human_C
         
         
         {
-            Console.WriteLine("Введите фамилию: ");
-            var surname = Console.ReadLine();
             
-            Console.WriteLine("Введите имя: ");
-            var name = Console.ReadLine();
+
             
-            Console.WriteLine("Введите отчество: ");
-            var patronymic = Console.ReadLine();
-            
-            Console.WriteLine("Введите дату рождения в формате ДД.ММ.ГГГГ: ");
-            var birth = Convert.ToDateTime(Console.ReadLine());
-            
-            List<Human> people = new List<Human>();
-
-            Console.Write("Выберите, кого добавить:\n1) student \n2) employee \n3) driver \nВаше решение: ");
-            var a = int.Parse(Console.ReadLine());
-            
-            switch (a)
-            {
-                case 1:
-                    Console.Write("Введите факультет");
-                    var faculty = Console.ReadLine();
-
-                    Console.Write("Введите курс");
-                    var course = int.Parse(Console.ReadLine());
-
-                    Console.Write("Введите группу");
-                    var group = int.Parse(Console.ReadLine());
-
-                    Student student = new Student(surname, name, patronymic, birth, faculty, course, group);
-
-                    people.Add(student);
-
-                    break;
-
-                case 2:
-                    Console.Write("Введите название компании");
-                    var company = Console.ReadLine();
-
-                    Console.Write("Введите заработную плату");
-                    var salary = int.Parse(Console.ReadLine());
-
-                    Console.Write("Введите опыт работы");
-                    var experience = int.Parse(Console.ReadLine());
-
-                    Employee employee = new Employee(surname, name, patronymic, birth, company, salary, experience);
-
-                    people.Add(employee);
-                    
-                    break;
-
-                case 3:
-                    Console.Write("Введите название компании");
-                    var company = Console.ReadLine();
-
-                    Console.Write("Введите заработную плату");
-                    var salary = int.Parse(Console.ReadLine());
-
-                    Console.Write("Введите опыт работы");
-                    var experience = int.Parse(Console.ReadLine());
-
-                    Console.Write("Введите бренд");
-                    var brand = Console.ReadLine();
-
-                    Console.Write("Введите модель");
-                    var model = Console.ReadLine();
-
-                    Driver driver = new Driver(surname, name, patronymic, birth, company, salary, experience, brand, model);
-                    
-                    people.Add(driver);
-                    
-                    break;
-            }
 
             for (var i = 0; i < people.Count; i++) // вывела на экран список людей и их индексы в списке
             {
